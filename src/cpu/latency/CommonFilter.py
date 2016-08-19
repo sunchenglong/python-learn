b079397a0723d8fe2151b2288235de34d1005a5b
suncl <suncl@ios.ac.cn>
2016-08-19 14:18:34 +0800

    FIX: format data

--- src/cpu/latency/CommonFilter.py
+++ src/cpu/latency/CommonFilter.py
@@ -8,9 +8,8 @@ Common Process Latency Data
 
 
 class CommonFilter:
-    def __init__(self, input = '../../../data/fvec.txt', output = '../../../output/fvec.txt'):
+    def __init__(self, input='../../../data/fvec.txt', output='../../../output/fvec.txt'):
         self.instructSet = set([])
-        self.type = {}
         self.clockDict = {}
         self.instructsDict = {}
         self.mean = {}
@@ -18,7 +17,7 @@ class CommonFilter:
         self.readLatency(input)
         self.query()
         self.write(output)
-        self.debug()
+        #self.debug()
 
     def readLatency(self, fileSrc):
         file = open(fileSrc, 'r')
@@ -27,70 +26,95 @@ class CommonFilter:
             if re.match('Latency: ', line) or re.match('Instruction: ', line) or re.match('instruction: ', line) \
                     or re.match('Throughput: ', line) or re.match('Latency/throughput: ', line) \
                     or re.match(
-                        '(SSE)|(taken)|(instructions)|(Instructions)|(Number)|(method: )'
-                        '|(Throughput)|(Latency)|(register)|(chainlength)|(multiplication)', line):
+                        '(taken)|(instructions)|(Instructions)|(method: )|(Throughput)|(Latency)', line):
                 if re.match('Latency: ', line):
                     instruct = line.split('Latency: ')[1].strip()
-                    self.type[instruct] = 'Latency'
+                    type = 'Latency'
                 elif re.match('Instruction: ', line):
                     instruct = line.split('Instruction: ')[1].strip()
-                    self.type[instruct] = 'Latency'
+                    type = 'Latency'
                 elif re.match('Latency/throughput: ', line):
                     instruct = line.split('Latency/throughput: ')[1].strip()
-                    self.type[instruct] = 'Latency'
+                    type = 'Latency'
                 elif re.match('instruction: ', line):
                     instruct = line.split('instruction: ')[1].strip()
-                    self.type[instruct] = 'Latency'
+                    type = 'Latency'
+                elif re.match('Throughput with memory operand: ', line):
+                    instruct = line.split('Throughput with memory operand: ')[1].strip()
+                    type = 'Throughput'
+                elif re.match('Throughput with memory source operand: ', line):
+                    instruct = line.split('Throughput with memory source operand: ')[1].strip()
+                    type = 'Throughput'
+                elif re.match('Throughput with memory destination operand: ', line):
+                    instruct = line.split('Throughput with memory destination operand: ')[1].strip()
+                    type = 'Throughput'
+                elif re.match('Latency with memory destination operand: ', line):
+                    instruct = line.split('Latency with memory destination operand: ')[1].strip()
+                    type = 'Latency'
                 elif re.match('Throughput: ', line):
                     instruct = line.split('Throughput: ')[1].strip()
-                    self.type[instruct] = 'Throughput'
+                    type = 'Throughput'
                 elif re.match(
-                        '(SSE)|(taken)|(instructions)|(Instructions)|(Number)|(method: )'
-                        '|(Throughput)|(Latency)|(register)|(chainlength)|(multiplication)', line):
+                        '(taken)|(instructions)|(Instructions)|(method: )|(Throughput)|(Latency)', line):
                     instruct = line.strip()
                     if re.match('(throughput)|(Throughput)', line):
-                        self.type[instruct] = 'Throughput'
+                        type = 'Throughput'
                     else:
-                        self.type[instruct] = 'Latency'
+                        type = 'Latency'
                 else:
                     print "nop"
                 print "LOG:" + instruct
+                print "LOG:" + type
                 self.instructSet.add(instruct)
-                self.clockDict[instruct] = []
-                self.instructsDict[instruct] = []
+                if instruct not in self.clockDict:
+                    self.clockDict[instruct] = {}
+                    self.instructsDict[instruct] = {}
+                self.clockDict[instruct][type] = []
+                self.instructsDict[instruct][type] = []
             elif re.match('( )*\d+( )*\d+', line):
                 m = re.match('( )*(\d+)( )*(\d+)', line)
                 try:
                     print instruct
-                    self.clockDict[instruct].append(int(m.group(2)))
-                    self.instructsDict[instruct].append(int(m.group(4)))
+                    print type
+                    self.clockDict[instruct][type].append(int(m.group(2)))
+                    self.instructsDict[instruct][type].append(int(m.group(4)))
                 except:
                     print "Except"
 
     def query(self):
         for key in self.instructSet:
-            if len(self.clockDict.get(key)) != 0:
-                clock = numpy.array(self.clockDict.get(key))
-                instructs = numpy.array(self.instructsDict.get(key))
-                self.mean[key] = 1.0 * clock.sum() / instructs.sum()
-                self.var[key] = (1.0 * clock / instructs).var()
+            if key not in self.mean:
+                self.mean[key] = {}
+                self.var[key] = {}
+            if 'Latency' in self.clockDict[key] and len(self.clockDict[key]['Latency']) != 0:
+                clock = numpy.array(self.clockDict[key]['Latency'])
+                instructs = numpy.array(self.instructsDict[key]['Latency'])
+                self.mean[key]['Latency'] = 1.0 * clock.sum() / instructs.sum()
+                self.var[key]['Latency'] = (1.0 * clock / instructs).var()
+            if 'Throughput' in self.clockDict[key] and len(self.clockDict[key]['Throughput']) != 0:
+                clock = numpy.array(self.clockDict[key]['Throughput'])
+                instructs = numpy.array(self.instructsDict[key]['Throughput'])
+                self.mean[key]['Throughput'] = 1.0 * clock.sum() / instructs.sum()
+                self.var[key]['Throughput'] = (1.0 * clock / instructs).var()
 
     def write(self, fileSrc):
         outFile = open(fileSrc, 'w')
         # outFile.write('Instruct name\tTest Type\tMean Latency(Clock Cycle)\tVar\n')
         for key in self.instructSet:
             try:
-                outFile.write(
-                    key + '\t' + self.type.get(key) + '\t' + str(round(self.mean.get(key), 2)) + '\t' + \
-                    str(round(self.var.get(key), 2)) + '\n')
+                throughput = str(round(self.mean[key]['Throughput'], 2))
             except:
-                print self.var.get(key)
-
+                throughput = 'miss'
+            try:
+                latency = str(round(self.mean[key]['Latency'], 2))
+            except:
+                latency = 'miss'
+            outFile.write(
+                    key + '\t' + latency + '\t' + throughput + '\n')
 
     def debug(self):
         for key in self.instructSet:
             print key
-            print self.type.get(key)
             print self.clockDict.get(key)
             print self.instructsDict.get(key)
             print self.mean.get(key)
@@ -100,6 +124,7 @@ class CommonFilter:
 class Driver:
     def __init__(self):
         self.listFiles('../../../result20160817', '../../../out20160817')
+        self.mergeFiles('../../../out20160817', '../../../output/result.xls')
 
     def listFiles(self, inputSrc, outputSrc):
         list = os.listdir(inputSrc)
@@ -108,9 +133,17 @@ class Driver:
             output = outputSrc + '/' + file
             print input
             print output
-            CommonFilter(input = input, output = output)
+            CommonFilter(input=input, output=output)
+
+    def mergeFiles(self, inputSrc, outputFile):
+        outputWriter = open(outputFile, 'w')
+        list = os.listdir(inputSrc)
+        for file in list:
+            input = inputSrc + '/' + file
+            inputReader = open(input, 'r')
+            outputWriter.write(inputReader.read())
 
 
 if __name__ == '__main__':
-    #commonFilter = CommonFilter(input = '../../../data/fvec.txt', output = '../../../output/fvec.txt')
+    # commonFilter = CommonFilter(input = '../../../data/fvec.txt', output = '../../../output/fvec.txt')
     driver = Driver()
---
+++ src/cpu/latency/FormatResult.py
@@ -0,0 +1,43 @@
+# coding=utf-8
+
+import re
+
+
+class FormatResult:
+    def __init__(self):
+        self.fileFormat('../../../output/result.xls', '../../../output/formatResult.xls')
+
+    def fileFormat(self, inputSrc, outputSrc):
+        inputFile = open(inputSrc, 'r')
+        outputFile = open(outputSrc, 'w')
+        outputFile.write('Instruct \t Operators \t Latency \t Throughput \t RawCommand \n')
+        for line in inputFile.readlines():
+            if re.match('(Throughput)|(Latency)|(Instructions)', line):
+                continue
+            try:
+                second = line.split('+')[1].strip()
+                first = line.split('+')[0].strip()
+                result = first.split(' ')[0].strip() + '+' + second.split(' ')[0].strip() \
+                         + '\t' + '' \
+                         + '\t' + line.split('\t')[1].strip() \
+                         + '\t' + line.split('\t')[2].strip() \
+                         + '\t' + line.split('\t')[0].strip()
+            except:
+                print line
+                try:
+                    result = line.split('\t')[0].split(' ')[0].strip() \
+                             + '\t' + line.split('\t')[0].split(' ', 1)[1] \
+                             + '\t' + line.split('\t')[1].strip() \
+                             + '\t' + line.split('\t')[2].strip() \
+                             + '\t' + line.split('\t')[0].strip()
+                except:
+                    result = line.split('\t')[0].split(' ')[0].strip() \
+                             + '\t' + '' \
+                             + '\t' + line.split('\t')[1].strip() \
+                             + '\t' + line.split('\t')[2].strip() \
+                             + '\t' + line.split('\t')[0].strip()
+            outputFile.write(result + '\n')
+
+
+if __name__ == '__main__':
+    formatResult = FormatResult()
